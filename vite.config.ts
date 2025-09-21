31da36fb199173dced3d8608a0f00b235569e90d
Mert48Ta <113031249+Mert48Ta@users.noreply.github.com>
2025-09-21 21:54:09 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',  // ← Netlify için çok önemli
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
