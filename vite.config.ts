7562528573fd3941bba55f8ad240dc6819f6edf5
Mert48Ta <mert.mert1@icloud.com>
2025-09-22 11:51:14 +0300

    yönlendirme

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects', // kökteki _redirects dosyan
+          dest: '.'          // build (dist) klasörünün köküne kopyalanacak
+        }
+      ]
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.webp'],
-});
+})
